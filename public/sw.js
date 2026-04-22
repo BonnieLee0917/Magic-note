3575aee25d94e700e5334cf4e59de4f32a87e5af
Bonnie-PM <bonnie@magic-note.dev>
2026-04-22 17:38:05 +0800

    fix: switch service worker to network-first strategy, bust old cache

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,26 @@
-const CACHE = 'magic-note-v1';
-const ASSETS = ['/', '/manifest.json', '/icon.svg'];
+const CACHE = 'magic-note-v2';
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  self.skipWaiting();
 });
+
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    const copy = response.clone();
-    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-    return response;
-  }).catch(() => cached)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
